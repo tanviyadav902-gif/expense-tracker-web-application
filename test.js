63a3a117a935accfe71069a8d1fe97e2bf19d40a
tanviyadav902-gif <tanviyadav902@gmail.com>
2026-06-19 17:44:35 +0530

    Update test.js

--- test.js
+++ test.js
@@ -1,6 +1,6 @@
 const { MongoClient, ServerApiVersion } = require('mongodb');
 
-const uri = "mongodb+srv://1234tanuyy_db_user:OJoHDrwWDdcOSvZt@cluster0.2jm9kh9.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+const uri = "mongodb+srv://1234tanuyy_db_user:oqS6qKTH4eU8IIxg@cluster0.2jm9kh9.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
 
 const client = new MongoClient(uri, {
   serverApi: {
